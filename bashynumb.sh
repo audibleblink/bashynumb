e113edcf917b32e8a470c4346b3046ae54dca784
Alex Flores <domain@me.com>
2017-08-30 21:58:20 -0400

    fixes redirected output formatting
      - conditionally assign color variables so redirected
        output is clean, but still colorful if run directly
      - set ENV path at the top of the script so full paths
        aren't necessary

--- bashynumb.sh
+++ bashynumb.sh
@@ -1,20 +1,19 @@
 #!/usr/bin/env bash
 
-# Thanks to arr0way for the section and banner code and some of the 
+# Shoutout to arr0way for the section and banner code and some of the
 # enum calls - https://highon.coffee/blog/linux-local-enumeration-script
 
-BLACK="\033[30m"
-RED="\033[31m"
-GREEN="\033[32m"
-YELLOW="\033[33m"
-BLUE="\033[34m"
-PINK="\033[35m"
-CYAN="\033[36m"
-WHITE="\033[37m"
-NORMAL="\033[0;39m"
+export PATH=/usr/bin:/bin:/usr/sbin:/sbin
+if [ -t 1 ]; then # stdout is a term
+  RED="\033[31m"
+  YELLOW="\033[33m"
+  BLUE="\033[34m"
+  CYAN="\033[36m"
+  NORMAL="\033[0;39m"
+fi
 
-printf "$GREEN"
-/bin/cat << "EOF"
+printf "$CYAN"
+cat << "EOF"
   _                _                             _
  | |              | |                           | |
  | | _   ____  ___| | _  _   _ ____  _   _ ____ | | _
@@ -49,74 +48,74 @@ function section() {
 }
 
 section "Current User Info"
-/usr/bin/id
+id
 echo on
-/bin/hostname
+hostname
 
 section "Linux Version"
-/bin/cat /etc/issue;
+cat /etc/issue;
 echo
-/bin/cat /etc/*-release
+cat /etc/*-release
 
 section "Kernel Info"
-/bin/uname -ar
+uname -ar
 
 section "Network Info"
-/bin/cat /etc/network/interfaces || /bin/cat /etc/sysconfig/network
+cat /etc/network/interfaces || cat /etc/sysconfig/network
 echo
-/bin/cat /etc/resolv.conf
+cat /etc/resolv.conf
 echo
 ifconfig -a || ip addr
 echo
 route || ip route
 
 section "Active Listening Services"
-/bin/netstat -tulpn 
+netstat -tulpn
 
 section "File System Info"
-/bin/df -h
+df -h
 echo
-/bin/mount | column -t
+mount | column -t
 echo
-/bin/cat /etc/fstab
+cat /etc/fstab
 
 section "Users"
-/bin/cat /etc/passwd
+cat /etc/passwd
 
 section "Groups"
-/bin/cat /etc/group
+cat /etc/group
 
 section "Cron Info"
-/bin/ls -lah /etc/cron*
+ls -lah /etc/cron*
 echo
-/bin/cat /etc/crontab
+cat /etc/crontab
 echo
-/usr/bin/crontab -l
+crontab -l
 
 section "SUID Files"
-/usr/bin/find / -perm -g=s -o -perm -4000 ! -type l -exec ls -ld {} \; 2>/dev/null | column -t
+find / -perm -g=s -o -perm -4000 ! -type l -exec ls -ld {} \; 2>/dev/null | column -t
 
 section "Root-owned, Writable and Executable Files"
-/usr/bin/find / -type f -executable -user root -writable -exec ls -ld {} \; 2>/dev/null | column -t
+find / -type f -executable -user root -writable -exec ls -ld {} \; 2>/dev/null | column -t
 
 section "Root-owned, Writable Files"
-/usr/bin/find / -type f -user root -writable -exec ls -ld {} \; 2>/dev/null | column -t
+find / -type f -user root -writable -exec ls -ld {} \; 2>/dev/null | column -t
 
 section "World Writable Directories"
-/usr/bin/find / -perm -222 -type d -exec ls -ld {} \; 2>/dev/null | column -t
+find / -perm -222 -type d -exec ls -ld {} \; 2>/dev/null | column -t
 
 section "World Writable Files"
-/usr/bin/find / -type f -perm 0777 -exec ls -lah {} \; 2>/dev/null | column -t
+find / -type f -perm 0777 -exec ls -lah {} \; 2>/dev/null | column -t
 
 section "Commands User Can Run with sudo"
-test [ echo 1 | sudo -S -l ] 2>/dev/null || echo "${USER} has no passwordless sudo commands configured"
+test [ echo '' | sudo -S -l ] 2>/dev/null || echo "${USER} has no passwordless sudo commands configured"
 
 section "Current User Sessions"
-/usr/bin/w
+w
 
 section "Processes Running as root"
-/bin/ps -ef | /bin/grep root | /bin/grep -ve "\]$"
+ps -ef | grep root | grep -ve "\]$"
 
 section "Installed Packages"
-/usr/bin/dpkg --list
+dpkg --list
 
